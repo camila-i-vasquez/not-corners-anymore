0ba637c174c925478e0e5d4278e6bdfea5e65deb
camila vasquez <camila.vasquez@gmail.com>
2019-03-18 14:20:26 -0700

    'created' hdf 'file'

--- data_exploration.py
+++ data_exploration.py
@@ -1,5 +1,14 @@
 import h5py
-with h5py.File('nyu_depth_data_labeled.mat', 'r') as f:
-    f.keys()
+import pandas as pd
+nyu_hdf = h5py.File('nyu_depth_data_labeled.mat', 'r')
 
 
+#print(list(file.keys()))
+# note that the kets are:['#refs#', '#subsystem#', 'accelData', 'depths', 'images', 'labels', 'names', 'namesToIds', 'rawDepths', 'scenes']
+#accel = file['accelData']
+#print(type(accel))
+#dset = file['nyu_depth_data_labeled.mat']
+#file.keys()
+
+#with h5py.File('nyu_depth_data_labeled.mat', 'r') as f:
+   # f.write("nyu_hdf")
