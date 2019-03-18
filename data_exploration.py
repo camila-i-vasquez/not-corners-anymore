6fbab9cc4b20443e6e401e1c543f7ca8e001b57e
camila vasquez <camila.vasquez@gmail.com>
2019-03-18 16:31:40 -0700

    made 1 subfile into a dataframe

--- data_exploration.py
+++ data_exploration.py
@@ -1,6 +1,27 @@
 import h5py
 import pandas as pd
+from scipy.io import loadmat
+import numpy as np
 nyu_hdf = h5py.File('nyu_depth_data_labeled.mat', 'r')
+accelData = nyu_hdf['accelData'][()]
+accelData = np.transpose(accelData)
+depths = nyu_hdf['depths'][()]
+# images = nyu_hdf['images'][()]
+# names = nyu_hdf['names'][()]
+# namesToIds=nyu_hdf['namesToIds'][()]
+# rawDepths =nyu_hdf['rawDepths'][()]
+# scenes =nyu_hdf['scenes'][()]
+names = ['accelData', 'depths', 'images','names','namesToIds', 'rawDepths', 'scenes']
+accelLabels = ['roll', 'yaw',  'pitch', 'tilt']
+depthsLabels = ['height', 'width', 'N']
+imageLabels =['height', 'width', 'RGB', 'N']
+accelData = pd.DataFrame(np.array(accelData), columns=accelLabels)
+#depths = pd.DataFrame(np.array(depths), columns = depthsLabels)
+
+
+
+
+
 
 
 #print(list(file.keys()))
