d1a0e6c1da4a12c89c62899e3b42bf76fbff741b
camila vasquez <camila.vasquez@gmail.com>
2019-03-19 13:39:32 -0700

    created method to process all the matlab files into arrays

--- data_exploration.py
+++ data_exploration.py
@@ -1,35 +1,25 @@
 import h5py
 import pandas as pd
-from scipy.io import loadmat
 import numpy as np
-nyu_hdf = h5py.File('nyu_depth_data_labeled.mat', 'r')
-accelData = nyu_hdf['accelData'][()]
-accelData = np.transpose(accelData)
-depths = nyu_hdf['depths'][()]
-# images = nyu_hdf['images'][()]
-# names = nyu_hdf['names'][()]
-# namesToIds=nyu_hdf['namesToIds'][()]
-# rawDepths =nyu_hdf['rawDepths'][()]
-# scenes =nyu_hdf['scenes'][()]
-names = ['accelData', 'depths', 'images','names','namesToIds', 'rawDepths', 'scenes']
-accelLabels = ['roll', 'yaw',  'pitch', 'tilt']
-depthsLabels = ['height', 'width', 'N']
-imageLabels =['height', 'width', 'RGB', 'N']
-accelData = pd.DataFrame(np.array(accelData), columns=accelLabels)
-#depths = pd.DataFrame(np.array(depths), columns = depthsLabels)
 
-
-
-
-
-
-
-#print(list(file.keys()))
-# note that the kets are:['#refs#', '#subsystem#', 'accelData', 'depths', 'images', 'labels', 'names', 'namesToIds', 'rawDepths', 'scenes']
-#accel = file['accelData']
-#print(type(accel))
-#dset = file['nyu_depth_data_labeled.mat']
-#file.keys()
-
-#with h5py.File('nyu_depth_data_labeled.mat', 'r') as f:
-   # f.write("nyu_hdf")
+def dataProcessing(filename):
+   file = h5py.File(filename, 'r')
+   my_dict = {}
+   my_dict['accelData'] = ['roll', 'yaw',  'pitch', 'tilt']
+   my_dict['depths']=['height', 'width', 'N']
+   my_dict['images'] =['height', 'width', 'RGB', 'N']
+   my_dict['labels']=['height', 'width', 'N']
+   my_dict['names']=['names']
+   my_dict['namesToIds']=['name', 'ID']
+   my_dict['rawDepths']=['height','width', 'N']
+   my_dict['scenes']=['image_scene']
+   dframe_names =['accelData', 'depths','images', 'labels', 'names', 'namesToIds','rawDepths', 'scenes']
+   accelData=file['accelData'][()]
+   depths = file['depths'][()]
+   images = file['images'][()]
+   labels = file['labels'][()]
+   names = file['names'][()]
+   namesToIds = file['namesToIds'][()]
+   rawDepths = file['rawDepths'][()]
+   scenes =file['scenes'][()]
+   return accelData,depths,images,names,namesToIds, rawDepths, scenes, my_dict
